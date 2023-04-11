9f8bd4e20dc7748d54f386b098c45a5c28b30a44
Redouan Bouziza <redouan.bouziza@hva.nl>
2023-04-11 14:53:51 +0200

    Refactor | Temp branch

--- app/src/main/java/com/example/vu/ui/screens/Screen.kt
+++ app/src/main/java/com/example/vu/ui/screens/Screen.kt
@@ -3,7 +3,7 @@ package com.example.vu.ui.screens
 sealed class Screen(
     val route: String
 ) {
-    object HomePage: Screen("home_page")
-    object ChartPage: Screen("chart_page")
+    object Home: Screen("home")
+    object Chart: Screen("chart")
 }
 
