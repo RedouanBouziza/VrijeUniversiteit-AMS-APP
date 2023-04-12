ab739d5fb9adddb1b18b362461e4da0dae31328f
casey kruijer <caseykruijer@gmail.com>
2023-04-12 13:23:52 +0200

    expandable image

--- app/src/main/java/com/example/vu/ui/screens/breathing/BreathingExcercise.kt
+++ app/src/main/java/com/example/vu/ui/screens/breathing/BreathingExcercise.kt
@@ -1,9 +1,52 @@
 package com.example.vu.ui.screens.breathing
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.vu.R
+
+enum class BreathingExcercisePosition {
+    Start, Finish
+}
 
 @Composable
-fun BreathingExercise(){
-    Text("Breathing Exercise")
+fun BreathingExercise() {
+    var imageState by remember { mutableStateOf(BreathingExcercisePosition.Start) }
+
+    val offsetAnimation: Dp by animateDpAsState(
+        if (imageState == BreathingExcercisePosition.Start) 200.dp else 250.dp
+    )
+
+    Column(
+        Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(R.drawable.breathe),
+            contentDescription = "",
+            modifier = Modifier
+                .height(offsetAnimation)
+                .width(offsetAnimation)
+        )
+
+        Button(onClick = {
+            imageState = when (imageState) {
+                BreathingExcercisePosition.Start -> BreathingExcercisePosition.Finish
+                BreathingExcercisePosition.Finish -> BreathingExcercisePosition.Start
+            }
+        }) {
+            Text("click")
+        }
+    }
 }
