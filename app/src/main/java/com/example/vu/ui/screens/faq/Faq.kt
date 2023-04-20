f8ceca8630808cc491cc6ef5ab0601cae2839ef2
Kaan Uğur <kaan.ugur@hva.nl>
2023-04-20 11:57:30 +0200

    Made the faq page

--- app/src/main/java/com/example/vu/ui/screens/faq/Faq.kt
+++ app/src/main/java/com/example/vu/ui/screens/faq/Faq.kt
@@ -1,10 +1,99 @@
 package com.example.vu.ui.screens.faq
 
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.vu.R
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun Faq(navController: NavHostController) {
-    Text(text = "Faq Screen")
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxWidth()
+            .padding(vertical = 32.dp)
+    ) {
+        Text(
+            text = "FAQ",
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            color = colorResource(id = R.color.ams),
+            fontSize = 32.sp
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Column(modifier = Modifier.padding(horizontal = 24.dp)) {
+            FaqItem(
+                "What is Lorem Ipsum?",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            )
+            FaqItem(
+                "Where does it come from?",
+                "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
+            )
+            FaqItem(
+                "Why do we use it?",
+                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here."
+            )
+            FaqItem(
+                "Where can I get some?",
+                "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+            )
+            FaqItem(
+                "What is Lorem Ipsum?",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            )
+        }
+    }
+}
+
+@Composable
+fun FaqItem(title: String, content: String) {
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Column(modifier = Modifier.clickable { expanded = !expanded }) {
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    modifier = Modifier.weight(1f)
+                )
+                Icon(
+                    if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = null,
+                    tint = MaterialTheme.colors.onSurface
+                )
+            }
+            if (expanded) {
+                Text(
+                    text = content,
+                    modifier = Modifier.padding(16.dp),
+                    fontSize = 14.sp,
+                    lineHeight = 20.sp
+                )
+            }
+        }
+    }
 }
