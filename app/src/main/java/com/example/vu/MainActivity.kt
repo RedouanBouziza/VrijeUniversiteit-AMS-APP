315d04f10c05dd27696f62ab39ce6363379f6466
Casey Kruijer <casey.kruijer@hva.nl>
2023-04-25 13:38:03 +0200

    hope this work

--- app/src/main/java/com/example/vu/MainActivity.kt
+++ app/src/main/java/com/example/vu/MainActivity.kt
@@ -78,6 +78,7 @@ class MainActivity : ComponentActivity() {
                     MyComposableFunction(scope)
                     ScreenContent(Modifier, scope)
                 }
+                
             }
         }
     }
